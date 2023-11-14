1fdcb3b2294d306ae48755d5e20643c4ba3a6d7d
Brendom Gonçalves <brendom.vi@gmail.com>
2023-11-14 15:41:20 -0300

    Pratica em aual

--- Semana_2/PraticaSala/Program.cs
+++ Semana_2/PraticaSala/Program.cs
@@ -1,5 +1,12 @@
-﻿string[] pessoas = {"Maria", "Joao", "Marcelo", "Fabricio", "Ana"};
+﻿#region foreach
+  string[] pessoas = {"Maria", "Joao", "Marcelo", "Fabricio", "Ana"};
 
-foreach(string pessoa in pessoas){
-  Console.WriteLine(pessoa);
-}
+  foreach(string pessoa in pessoas){
+    Console.WriteLine(pessoa);
+  }
+#endregion
+
+#region Name
+  string? nome = Console.ReadLine();
+  Console.WriteLine($"Nome digitado: {nome}");
+#endregion
