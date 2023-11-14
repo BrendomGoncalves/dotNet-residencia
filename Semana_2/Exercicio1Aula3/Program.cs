b5d04ec826c9109d527412db2f8dd2452e0de52b
Brendom Gonçalves <brendom.vi@gmail.com>
2023-11-14 15:22:11 -0300

    Exercicio em aula

---
+++ Semana_2/Exercicio1Aula3/Program.cs
@@ -0,0 +1,24 @@
+﻿#region Questao1
+  Console.WriteLine("Numeros divisiveis por 3 e 4:");
+  for(int i = 0; i<=30; i++){
+    if(i % 3 == 0) Console.WriteLine(i);
+    else if(i % 4 == 0) Console.WriteLine(i);
+  }
+#endregion
+
+#region Questao2
+  Console.WriteLine("Sequencia de Fibonacci");
+  Console.WriteLine("0");
+  int anterior = 0, proximo = 1;
+  do{
+    Console.WriteLine(proximo);
+    int tmp = anterior;
+    anterior = proximo;
+    proximo += tmp;
+  }while(proximo <= 100);
+#endregion
+
+#region Questao3
+  Console.WriteLine("Tabela:");
+  
+#endregion
---
+++ Semana_2/PraticaSala/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] pessoas = {"Maria", "Joao", "Marcelo", "Fabricio", "Ana"};
+
+foreach(string pessoa in pessoas){
+  Console.WriteLine(pessoa);
+}
