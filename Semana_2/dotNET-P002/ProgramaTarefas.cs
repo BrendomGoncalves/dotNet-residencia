f7f515a0d613fa4a7598380f4b0d8e18b6bf5c2c
Brendom Gonçalves <brendom.vi@gmail.com>
2023-11-14 22:27:13 -0300

    Refatorando função listarTarefas, adicionando menu

--- Semana_2/dotNET-P002/ProgramaTarefas.cs
+++ Semana_2/dotNET-P002/ProgramaTarefas.cs
@@ -10,7 +10,7 @@ class ProgramaTarefas
       Console.Clear();
       Console.WriteLine("GERENCIADOR DE TAREFAS");
       Console.WriteLine("1. Nova Tarefa");
-      Console.WriteLine("2. Listar Todas as Tarefas");
+      Console.WriteLine("2. Listar Tarefas");
       Console.Write("5. Exit\n> ");
       opcao = Console.ReadKey().KeyChar;
       Console.WriteLine();
@@ -46,22 +46,82 @@ class ProgramaTarefas
   }
   public static void listarTarefas()
   {
+    char? opcao;
+
     Console.Clear();
-    Console.WriteLine("LISTA DE TAREFAS CRIADAS:\n");
-    if (tarefas.Count == 0)
+    Console.WriteLine("GERENCIADOR DE TAREFAS");
+    Console.WriteLine("1. Listar Todas as Tarefas Criadas");
+    Console.WriteLine("2. Listar Tarefas Concluídas");
+    Console.Write("3. Listar Tarefas Pendentes\n> ");
+    opcao = Console.ReadKey().KeyChar;
+
+    bool existeTarefas = false;
+
+    switch (opcao)
     {
-      Console.WriteLine("Nenhuma tarefa criada...");
-    }
-    else
-    {
-      foreach (Tarefa tarefa in tarefas)
-      {
-        if (tarefa.StatusConclusao) Console.Write("CONCLUIDA - ");
-        else Console.Write("PENDENTE - ");
-        Console.WriteLine(tarefa.Titulo + " (" + tarefa.DataVencimento.ToString("dddd | dd/MM/yyyy | HH:mm", new CultureInfo("pt-BR")) + ")");
-        Console.WriteLine("Descrição: " + tarefa.Descricao);
-      }
+      case '1':
+        Console.Clear();
+        Console.WriteLine("LISTA DE TAREFAS CRIADAS:\n");
+        if (tarefas.Count == 0)
+        {
+          Console.WriteLine("Nenhuma tarefa criada...");
+        }
+        else
+        {
+          foreach (Tarefa tarefa in tarefas)
+          {
+            if (tarefa.StatusConclusao) Console.Write("CONCLUIDA - ");
+            else Console.Write("PENDENTE - ");
+            Console.WriteLine(tarefa.Titulo + " (" + tarefa.DataVencimento.ToString("dddd | dd/MM/yyyy | HH:mm", new CultureInfo("pt-BR")) + ")");
+            Console.WriteLine("Descrição: " + tarefa.Descricao + "\n");
+          }
+        }
+        break;
+      case '2':
+        Console.Clear();
+        Console.WriteLine("LISTA DE TAREFAS CONCLUIDAS:\n");
+        if (tarefas.Count == 0)
+        {
+          Console.WriteLine("Nenhuma tarefa criada...");
+        }
+        else
+        {
+          foreach (Tarefa tarefa in tarefas)
+          {
+            if (tarefa.StatusConclusao){
+              Console.Write("CONCLUIDA - ");
+              Console.WriteLine(tarefa.Titulo + " (" + tarefa.DataVencimento.ToString("dddd | dd/MM/yyyy | HH:mm", new CultureInfo("pt-BR")) + ")");
+              Console.WriteLine("Descrição: " + tarefa.Descricao + "\n");
+              existeTarefas = true;
+            }
+          }
+          if (!existeTarefas) Console.WriteLine("Ainda não concluiu nenhuma tarefa...");
+        }
+        break;
+      case '3':
+        Console.Clear();
+        Console.WriteLine("LISTA DE TAREFAS PENDENTES:\n");
+        if (tarefas.Count == 0)
+        {
+          Console.WriteLine("Nenhuma tarefa criada...");
+        }
+        else
+        {
+          foreach (Tarefa tarefa in tarefas)
+          {
+            if (!tarefa.StatusConclusao){
+              Console.Write("PENDENTE - ");
+              Console.WriteLine(tarefa.Titulo + " (" + tarefa.DataVencimento.ToString("dddd | dd/MM/yyyy | HH:mm", new CultureInfo("pt-BR")) + ")");
+              Console.WriteLine("Descrição: " + tarefa.Descricao + "\n");
+              existeTarefas = true;
+            }
+          }
+          if (!existeTarefas) Console.WriteLine("Todas as tarefas já foram concluidas...");
+        }
+        break;
+      default:
+        Console.WriteLine("\nOpção inválida, escolha uma opção existente!");
+        break;
     }
   }
-
 }
