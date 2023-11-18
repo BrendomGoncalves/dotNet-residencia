2b63901fe016c67391c0018dbc98908c15b2c0e8
Brendom Gonçalves <brendom.vi@gmail.com>
2023-11-18 18:42:35 -0300

    Conclusão da estatisticas e finalização do programa

--- Semana_2/dotNET-P002/ProgramaTarefas.cs
+++ Semana_2/dotNET-P002/ProgramaTarefas.cs
@@ -3,12 +3,15 @@ class ProgramaTarefas
 {
   private static List<Tarefa> tarefasConcluidas = new List<Tarefa>();
   private static List<Tarefa> tarefasPendentes = new List<Tarefa>();
+  private static bool tarefasModificada = false;
   static void Main()
   {
     char opcao;
     do
     {
       Console.Clear();
+      if (tarefasConcluidas.Count > 0 || tarefasPendentes.Count > 0) Console.WriteLine($"Mais recente: {tempoTarefa("r")}\nMais antiga: {tempoTarefa("a")}");
+      Console.WriteLine($"[Pendentes: {tarefasPendentes.Count} | Concluidas: {tarefasConcluidas.Count} | Total: {tarefasPendentes.Count + tarefasConcluidas.Count}]");
       Console.WriteLine("GERENCIADOR DE TAREFAS");
       Console.WriteLine("1. Nova Tarefa");
       Console.WriteLine("2. Concluir Tarefa");
@@ -37,6 +40,7 @@ class ProgramaTarefas
             else { }
             tarefasPendentes.Add(novaTarefa);
             Console.WriteLine("\nTarefa criada com sucesso!");
+            tarefasModificada = true;
           }
           break;
         case '2':
@@ -185,6 +189,7 @@ class ProgramaTarefas
           //tarefasPendentes.RemoveAt(idTarefa - 1);
           tarefasPendentes.Remove(tarefasPendentes[idTarefa - 1]);
           Console.WriteLine("\nTarefa concluida com sucesso!");
+          tarefasModificada = true;
 
           Console.Write("\nDeseja concluir mais tarefas? [S/N]\n> ");
           opcao = Console.ReadKey().KeyChar;
@@ -236,6 +241,7 @@ class ProgramaTarefas
             {
               tarefasConcluidas.RemoveAt(idTarefa - 1);
               Console.WriteLine("\nTarefa removida com sucesso!");
+              tarefasModificada = true;
             }
             break;
           case 2:
@@ -260,6 +266,7 @@ class ProgramaTarefas
             {
               tarefasPendentes.RemoveAt(idTarefa - 1);
               Console.WriteLine("\nTarefa removida com sucesso!");
+              tarefasModificada = true;
             }
             break;
           case 0:
@@ -293,7 +300,7 @@ class ProgramaTarefas
     Console.WriteLine("\nTarefas Pendentes:");
     foreach (Tarefa tarefa in tarefasPendentes)
     {
-      if (tarefa.Titulo != null && tarefa.Descricao != null && (tarefa.Titulo.Contains(pesquisa) || tarefa.Descricao.Contains(pesquisa)))
+      if (tarefa.Titulo != null && tarefa.Descricao != null && (tarefa.Titulo.ToLower().Contains(pesquisa.ToLower()) || tarefa.Descricao.ToLower().Contains(pesquisa.ToLower())))
       {
         Console.Write("PENDENTE - ");
         Console.WriteLine(tarefa.Titulo + " (" + tarefa.DataVencimento.ToString("dddd | dd/MM/yyyy | HH:mm", new CultureInfo("pt-BR")) + ")");
@@ -301,12 +308,12 @@ class ProgramaTarefas
         existePendente = true;
       }
     }
-    if(!existePendente) Console.WriteLine("Nenhuma tarefa pendente com esse termo...");
+    if (!existePendente) Console.WriteLine("Nenhuma tarefa pendente com esse termo...");
 
-    Console.WriteLine("Tarefas Concluídas:");
+    Console.WriteLine("\nTarefas Concluídas:");
     foreach (Tarefa tarefa in tarefasConcluidas)
     {
-      if (tarefa.Titulo != null && tarefa.Descricao != null && (tarefa.Titulo.Contains(pesquisa) || tarefa.Descricao.Contains(pesquisa)))
+      if (tarefa.Titulo != null && tarefa.Descricao != null && (tarefa.Titulo.ToLower().Contains(pesquisa.ToLower()) || tarefa.Descricao.ToLower().Contains(pesquisa.ToLower())))
       {
         Console.Write("CONCLUIDA - ");
         Console.WriteLine(tarefa.Titulo + " (" + tarefa.DataVencimento.ToString("dddd | dd/MM/yyyy | HH:mm", new CultureInfo("pt-BR")) + ")");
@@ -314,6 +321,40 @@ class ProgramaTarefas
         existeConcluida = true;
       }
     }
-    if(!existeConcluida) Console.WriteLine("Nenhuma tarefa concluída com esse termo...");
+    if (!existeConcluida) Console.WriteLine("Nenhuma tarefa concluída com esse termo...");
+  }
+  public static string tempoTarefa(string parametro)
+  {
+    Tarefa tempTarefa;
+
+    if(!tarefasModificada){
+      tarefasConcluidas.Sort(CompararTarefasPorDataVencimento);
+      tarefasPendentes.Sort(CompararTarefasPorDataVencimento);
+      tarefasModificada = false;
+    }
+
+    if (parametro == "r")
+    {
+      if(tarefasPendentes.Count > 0 && tarefasConcluidas.Count > 0)
+        tempTarefa = (tarefasPendentes[0].DataVencimento > tarefasConcluidas[0].DataVencimento) ? tarefasPendentes[0] : tarefasConcluidas[0];
+      else if(tarefasPendentes.Count > 0 && tarefasConcluidas.Count == 0) tempTarefa = tarefasPendentes[0];
+      else tempTarefa = tarefasConcluidas[0];
+
+      return $"{tempTarefa.Titulo} [{tempTarefa.DataVencimento.ToString("dd/MM")}]";
+    }
+    else if (parametro == "a")
+    {
+      int indexUltimoP = tarefasPendentes.Count - 1, indexUltimoC = tarefasConcluidas.Count - 1;
+      if(tarefasPendentes.Count > 0 && tarefasConcluidas.Count > 0)
+        tempTarefa = (tarefasPendentes[indexUltimoP].DataVencimento < tarefasConcluidas[indexUltimoC].DataVencimento) ? tarefasPendentes[indexUltimoP] : tarefasConcluidas[indexUltimoC];
+      else if(tarefasPendentes.Count > 0 && tarefasConcluidas.Count == 0) tempTarefa = tarefasPendentes[indexUltimoP];
+      else tempTarefa = tarefasConcluidas[indexUltimoC];
+
+      return $"{tempTarefa.Titulo} [{tempTarefa.DataVencimento.ToString("dd/MM")}]";
+    } else return "Sem tarefas";
+  }
+  private static int CompararTarefasPorDataVencimento(Tarefa t1, Tarefa t2)
+  {
+    return t1.DataVencimento.CompareTo(t2.DataVencimento);
   }
 }
