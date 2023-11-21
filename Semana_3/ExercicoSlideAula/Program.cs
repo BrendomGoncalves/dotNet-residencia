858013e5958a45e31d521d39841c5a0a54562521
Brendom <brendom.vi@gmail.com>
2023-11-21 14:18:10 -0300

    Exercicio de tuplas

---
+++ Semana_3/ExercicoSlideAula/Program.cs
@@ -0,0 +1,22 @@
+﻿#region Tupla Exemplo
+  var tuple1 = (10, 20);
+  Console.WriteLine($"Tupla 1: {tuple1.Item1},  {tuple1.Item2}");
+#endregion
+
+#region Tupla Exemplo 2
+  var tuple2 = (x: 5, y: 20);
+  Console.WriteLine($"Tupla 2: {tuple2.x},  {tuple2.y}");
+#endregion
+
+#region Tupla Exemplo 3
+  var tuple3 = (id: 10, name: "Helder", nascimento: new DateTime(1999, 5, 20));
+  Console.WriteLine($"Tupla 3: {tuple3.id}, {tuple3.name}, {tuple3.nascimento}");
+#endregion
+
+#region Tupla Exemplo 4
+  List<(int id, string name, DateTime nascimento)> lista = new();
+  lista.Add(tuple3);
+  lista.Add((11, "Nicole", new DateTime(2019, 1, 12)));
+  Console.WriteLine("Tupla 4:");
+  lista.ForEach(x => Console.WriteLine($"- {x.id}, {x.name}, {x.nascimento.ToString("dd/MM/yyyy")}"));
+#endregion
