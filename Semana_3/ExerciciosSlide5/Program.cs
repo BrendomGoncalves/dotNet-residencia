c0dbca0f05bdae8b07a53fe142f8a49ed7f8b3d7
Brendom <brendom.vi@gmail.com>
2023-11-21 17:29:38 -0300

    Exeercicios slide aula 5

--- Semana_3/ExerciciosSlide5/Program.cs
+++ Semana_3/ExerciciosSlide5/Program.cs
@@ -1,4 +1,5 @@
 ﻿#region Tuplas
+  Console.WriteLine("-- Tuplas: ");
   (string, int) SuaIdade(string name, DateTime aniversario){
     int age = DateTime.Today.Year - aniversario.Year;
     if(DateTime.Today.DayOfYear < aniversario.DayOfYear) age--;
@@ -14,15 +15,27 @@
 #endregion
 
 #region Expressões Lambda
-  
+  Console.WriteLine("-- Expressões Lambda: ");
+  Func<int, int> somaDosQuadrados = x => x*x + x*x;
+  Console.WriteLine($"Soma dos quadrados de 10: {somaDosQuadrados(10)}");
+  Console.WriteLine($"Soma dos quadrados de 5: {somaDosQuadrados(5)}");
+  Console.WriteLine($"Soma dos quadrados de 50: {somaDosQuadrados(50)}");
 #endregion
 
 #region LINQ com Lista
-  
+  Console.WriteLine("-- LINQ com Lista: ");
+  List<(string nome, int idade)> dadosPessoas = new();
+  dadosPessoas.Add((nome: "Bruna", idade: 30));
+  dadosPessoas.Add((nome: "Maria", idade: 46));
+  dadosPessoas.Add((nome: "Bruno", idade: 21));
+  dadosPessoas.Add((nome: "Brendom", idade: 24));
+  Console.WriteLine($"Pessoas com mais de 30 anos: {string.Join(", ", dadosPessoas.Where(x => x.idade >= 30).Select(x => x.nome))}");
 #endregion
 
 #region LINQ com Array
-  
+  Console.WriteLine("-- LINQ com Array: ");
+  int[] numerosInteiros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  Console.WriteLine($"Numeros pares: {string.Join(", ", numerosInteiros.Where(x => x % 2 == 0))}");
 #endregion
 
 #region Combinação de Tuplas, Expressõẽs Lambda e LINQ
