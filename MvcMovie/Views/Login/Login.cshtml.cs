53bad08f2ff48b389665a8b9a00fbea374c56857
BrendomGoncalves <brendom.vi@gmail.com>
2024-02-26 15:04:20 -0300

    Login e, Autenticação e Token

---
+++ MvcMovie/Views/Login/Login.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcMovie.Views.Login;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ MvcMovie/Views/Login/LoginSuccess.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcMovie.Views.Login;
+
+public class LoginSuccess : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
