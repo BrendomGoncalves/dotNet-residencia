79dc24a5557e83a6755c9fe8fca1173542f99d51
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-12 15:05:44 -0300

    Adiciona classes Lampada, ExercicioUm.csproj e Program.cs

---
+++ ExercicioUm/Classes/Lampada.cs
@@ -0,0 +1,31 @@
+namespace ExercicioUm.Classes;
+
+public class Lampada
+{
+    public bool Ligada { get;set; } = false;
+
+    public Lampada(bool _ligarLampada)
+    {
+        if(_ligarLampada)
+        {
+            this.Ligar();
+        }
+    }
+
+    public void Ligar(){
+        this.Ligada = true;
+    }
+    public void Desligar(){
+        this.Ligada = false;
+    }
+    public void ImprimirEstado(){
+        if(this.Ligada)
+        {
+            System.Console.WriteLine("Lâmpada ligada!");
+        }
+        else
+        {
+            System.Console.WriteLine("Lâmpada desligada!");
+        }
+    }
+}
---
+++ ExercicioUm/Program.cs
@@ -0,0 +1,8 @@
+﻿using ExercicioUm.Classes;
+
+Lampada lampadaSala = new Lampada(true);
+Lampada lampadaCozinha = new Lampada(false);
+
+System.Console.WriteLine("Estado das lâmpadas:");
+lampadaSala.ImprimirEstado();
+lampadaCozinha.ImprimirEstado();
