aae27c882a7975f855bc709b0dcbf165e2983c63
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-12 15:23:36 -0300

    Atualizar Program.cs com namespaces e formatação correta

--- ExercicioUm/Program.cs
+++ ExercicioUm/Program.cs
@@ -1,8 +1,14 @@
 ﻿using ExercicioUm.Classes;
 
-Lampada lampadaSala = new Lampada(true);
-Lampada lampadaCozinha = new Lampada(false);
+namespace ExercicioUm
+{
+    public class Program
+    {
+        Lampada lampadaSala = new Lampada(true);
+        Lampada lampadaCozinha = new Lampada(false);
 
-System.Console.WriteLine("Estado das lâmpadas:");
-lampadaSala.ImprimirEstado();
-lampadaCozinha.ImprimirEstado();
+        System.Console.WriteLine("Estado das lâmpadas:");
+        lampadaSala.ImprimirEstado();
+        lampadaCozinha.ImprimirEstado();
+    }
+}
