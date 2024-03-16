7d425077a44066380029325684f83647d81ed915
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-15 22:10:02 -0300

    Adiciona projeto Exercicio2.csproj e Program.cs com código para tratar NullReferenceException

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio2;
+
+class Program{
+    static void Main(string[] args){
+        object o = null;
+        try{
+            Console.WriteLine(o.ToString());
+        }catch(NullReferenceException e){
+            Console.WriteLine("Referência de objeto não definida como uma instância de um objeto.");
+        }
+    }
+}
