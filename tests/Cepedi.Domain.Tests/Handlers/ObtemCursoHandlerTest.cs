9f2b295ea2bcd4abaf9942f2b7f255acd1d6169b
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-20 20:08:56 -0300

    Atualiza versões do .NET Framework e adiciona referências de projeto e pacote

---
+++ tests/Cepedi.Domain.Tests/Handlers/ObtemCursoHandlerTest.cs
@@ -0,0 +1,60 @@
+﻿using Cepedi.Domain.Entities;
+using NSubstitute;
+
+namespace Cepedi.Domain.Tests;
+
+public class ObtemCursoHandlerTest
+{
+    private readonly ICursoRepository _cursoRepository = Substitute.For<ICursoRepository>();
+    private readonly IProfessorRepository _professorRepository = Substitute.For<IProfessorRepository>();
+    private readonly ObtemCursoHandler _sut;
+
+    public ObtemCursoHandlerTest()
+    {
+        _sut = new ObtemCursoHandler(_cursoRepository, _professorRepository);
+    }
+
+    [Fact]
+    public async Task ObterCursoAsync_QuandoObtido_DeveRetornarCursoEncontrado()
+    {
+        // Arrange
+        var curso = new CursoEntity
+        {
+            Nome = "Teste",
+            DataInicio = DateTime.Now,
+            DataFim = DateTime.Now,
+            ProfessorId = 1
+        };
+        var professor = new ProfessorEntity(1, "Teste", "Especialidade_Teste");
+        _cursoRepository.ObtemCursoPorIdAsync(1).ReturnsForAnyArgs(curso);
+        _professorRepository.ObtemProfessorPorIdAsync(1).ReturnsForAnyArgs(professor);
+
+        // Act
+        var result = await _sut.ObterCursoAsync(1);
+
+        // Assert
+        Assert.Equal("Teste", result.curso);
+    }
+
+    [Fact]
+    public async Task ObterCursosAsync_QuandoObtido_DeveRetornarUm()
+    {
+        // Arrange
+        var curso = new CursoEntity
+        {
+            Nome = "Teste",
+            DataInicio = DateTime.Now,
+            DataFim = DateTime.Now,
+            ProfessorId = 1
+        };
+        var professor = new ProfessorEntity(1, "Teste", "Especialidade_Teste");
+        _cursoRepository.ObtemCursosAsync().ReturnsForAnyArgs(new List<CursoEntity> { curso });
+        _professorRepository.ObtemProfessorPorIdAsync(1).ReturnsForAnyArgs(professor);
+
+        // Act
+        var result = await _sut.ObterCursosAsync();
+
+        // Assert
+        Assert.Equal("Teste", result.First().curso);
+    }
+}
