48c4bcefc07e47662ac5b2e848d5d9ce0afc044e
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-20 23:07:20 -0300

    Exercicio 4 Async/Await

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AsyncAwait;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var task1 = DoWorkAsync("Tarefa 1", 5);
+        var task2 = DoWorkAsync("Tarefa 2", 7);
+
+        await Task.WhenAll(task1, task2);
+
+        Console.WriteLine("Ambas as tarefas foram concluídas!");
+    }
+
+    private static async Task DoWorkAsync(string taskName, int delay)
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"{taskName} está em {i * 10}%");
+            await Task.Delay(delay * 1000);
+        }
+    }
+}
