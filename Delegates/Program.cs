2d97f7a77ae386d297900533a34f6e7e1918c253
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-20 22:50:12 -0300

    Exercicio 1 Delegates

---
+++ Delegates/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Delegates;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var elementosDouble = new List<double>{ 3, 7, 2, 4, 6 };
+        var elementosConvertidos = elementosDouble.ConvertAll(x => x/2);
+        elementosConvertidos.ForEach(Console.WriteLine);
+    }
+}
